ac651f47e0582b7afdfd804c0efdd3cf0d84b03a
KemCake <santos.remi@icloud.com>
2016-05-12 09:56:02 +0200

    Endpoints as a grid

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -82,7 +82,7 @@ kuiApp.controller("kuiController", function ($rootScope, $scope, $location, $rou
 
 kuiApp.controller("contextController", function ($scope, $location, config, NgTableParams) {
 
-    $scope.contexts = config.Contexts;
+    this.contexts = config.Contexts;
     this.openIndex = -1;
     this.tableParams = new NgTableParams({count: 5}, {counts: [5, 10, 25], data: $scope.contexts});
     $scope.clusters = config.Clusters;
